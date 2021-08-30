c310fd067e98688cce208791071a06bb3e88c539
hermione-napptive <cicd@napptive.com>
2021-08-30 11:04:58 +0000

    gpm automatic publish

--- entities.pb.validate.go
+++ entities.pb.validate.go
@@ -170,3 +170,72 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = OpResponseValidationError{}
+
+// Validate checks the field values on TokenResponse with the rules defined in
+// the proto definition for this message. If any rules are violated, an error
+// is returned.
+func (m *TokenResponse) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for Token
+
+	// no validation rules for RefreshToken
+
+	return nil
+}
+
+// TokenResponseValidationError is the validation error returned by
+// TokenResponse.Validate if the designated constraints aren't met.
+type TokenResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e TokenResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e TokenResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e TokenResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e TokenResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e TokenResponseValidationError) ErrorName() string { return "TokenResponseValidationError" }
+
+// Error satisfies the builtin error interface
+func (e TokenResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sTokenResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = TokenResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = TokenResponseValidationError{}
